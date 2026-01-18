51d20fd01ecfc6e015d95ed37ca266cf3cc38587
nutmegdev96 <macissimon@gmail.com>
2026-01-18 17:37:32 +0100

    Initialize tests module with metadata and imports
    Added metadata and imports for data cleaning toolkit.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,21 @@
+"""
+Data Cleaner Pro - Professional data cleaning toolkit
+"""
 
+__version__ = '1.1.0'
+__author__ = 'nutmegdev96'
+__license__ = 'MIT'
+__description__ = 'Professional data cleaning toolkit for everyday use'
+
+# Import main classes for easier access
+from .cleaner import DataCleaner
+from .transformers import DataTransformer
+from .validators import DataValidator
+
+# Export main classes
+__all__ = [
+    'DataCleaner',
+    'DataTransformer',
+    'DataValidator',
+    '__version__',
+]
